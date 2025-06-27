2abc93f365c9fa94d012f7ba669c1aedb8f40e85
St-thomas-Aquinas <maxkuria254@gmail.com>
2025-06-27 14:00:09 +0300

    Submitting the assingment for number guessing game

---
+++ src/App.tsx
@@ -0,0 +1,73 @@
+import { useReducer } from "react";
+import { useRef } from "react";
+import "./App.css";
+//variables
+let randomNumber: number = 0;
+
+//function to generate Random Number
+function random() {
+  randomNumber = Math.trunc(Math.random() * 100);
+  console.log(randomNumber);
+  // alert(ran)
+}
+random();
+//end of the function to generete random number
+
+const App = () => {
+  const inputRef = useRef(0);
+  const [results, dispatch] = useReducer(reducerFunction, {
+    feedback: "Start Game",
+    trial: 10,
+  });
+
+  function reducerFunction(
+    state: {
+      trial: number;
+      feedback: string;
+    },
+    action: { type: string }
+  ) {
+    if (action.type === "ChechNumber") {
+      if (randomNumber > inputRef.current.value) {
+        return {
+          ...state,
+          feedback: "Your guess is too low",
+          trial: state.trial - 1,
+        };
+      } else if (randomNumber < inputRef.current.value) {
+        return {
+          ...state,
+          feedback: "Your guess is too high",
+          trial: state.trial - 1,
+        };
+      } else if (randomNumber == inputRef.current.value) {
+        return { ...state, feedback: "You won the game" };
+      }
+    }
+  }
+  
+
+  return (
+    <div>
+      <h1>Welcome to the Number guessing game</h1>
+      <button disabled={results.trial == 0 ? false : true}>
+        New Game
+      </button>
+      <p>Trials: {results.trial}</p>
+      <input
+        ref={inputRef}
+        type="number"
+        disabled={results.trial == 0 ? true : false}
+      />
+      <p> {results.feedback}</p>
+      <button
+        onClick={() => dispatch({ type: "ChechNumber" })}
+        disabled={results.trial == 0 ? true : false}
+      >
+        Guess Number
+      </button>
+    </div>
+  );
+};
+
+export default App;
